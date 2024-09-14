6e24051daa4758bd715be67865bff9b43ddd70f7
Andy Phu <phu00003@umn.edu>
2024-09-14 16:22:01 -0400

    text white

--- app/canvas_sample/page.tsx
+++ app/canvas_sample/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="bg-transparent">
         <div className="bg-slate-500 rounded-lg h-[100px] w-[300px] justify-center items-center flex flex-row">
-            <h1> AI estimated work time: 40 minutes</h1>   
+            <h1 className="text-white"> AI estimated work time: 40 minutes</h1>   
         </div>
 
     </div>
